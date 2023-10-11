418cb6f3bea8ab4384500ec40e65f0d212474586
Austin <auserich@gmail.com>
2023-10-11 12:49:53 -0700

    Added landing page and route to it in App

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,6 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Replies from "./components/Replies";
-<<<<<<< HEAD
-import Profile from "./components/Profile";
-
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
-                    <Route path='/dashboard' element={<Home/>} />
-                    <Route path='/:id/replies' element={<Replies />} />
-                    <Route path='/profile' element={<Profile />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
-=======
 import Landing from "./components/Landing";
 
 const App = () => {
@@ -34,11 +16,11 @@ const App = () => {
 					<Route path="/register" element={<Register />} />
 					<Route path="/dashboard" element={<Home />} />
 					<Route path="/:id/replies" element={<Replies />} />
+                    <Route path='/profile' element={<Profile />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
 	);
->>>>>>> e7ea23c (Added landing page and route to it in App)
 };
 
 export default App;
