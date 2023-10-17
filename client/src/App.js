f12e0d898d05bce422157ca9405efcf45706208b
prodhanm <mustafizur.prodhan@gmail.com>
2023-10-17 11:25:07 -0400

    modified app.js and accepted styling pages.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Replies from "./components/Replies";
+<<<<<<< HEAD
 import Profile from "./components/Profile";
 
 const App = () => {
@@ -20,6 +21,24 @@ const App = () => {
             </BrowserRouter>
         </div>
     );
+=======
+import Landing from "./components/Landing";
+
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/landing" element={<Landing />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/dashboard" element={<Home />} />
+					<Route path="/:id/replies" element={<Replies />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+>>>>>>> e7ea23c (Added landing page and route to it in App)
 };
 
 export default App;
