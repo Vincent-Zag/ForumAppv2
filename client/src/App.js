6c61902b6669e95f9b05776cb6ebc56fe7a9998e
Austin <auserich@gmail.com>
2023-10-22 08:16:21 -0700

    WIP for styles

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,26 @@ import Login from "./components/Login";
 import Home from "./components/Home";
 import Replies from "./components/Replies";
 import Landing from "./components/Landing";
-import Profile from "./components/Profile"; 
+import Profile from "./components/Profile";
 import NavComponent from "./components/NavComponent";
 import "bootswatch/dist/quartz/bootstrap.min.css";
 
 const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/landing" element={<Landing />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} /> 
-          <Route path="/profile/:userId" element={<NavComponent />} />
-          <Route path="/:id/replies" element={<Replies />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/landing" element={<Landing />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/dashboard" element={<Home />} />
+					<Route path="/profile/:userId" element={<Profile />} />
+					<Route path="/profile/:userId" element={<NavComponent />} />
+					<Route path="/:id/replies" element={<Replies />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -37,30 +37,29 @@ const Home = () => {
 			.catch((err) => console.error(err));
 	}, [navigate]);
 
-  const createThread = () => {
-    fetch("http://localhost:4000/api/create/thread", {
-      method: "POST",
-      body: JSON.stringify({
-        thread,
-        userId: localStorage.getItem("userId"),
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          setThreadList([...threadList, data.thread]); 
-          setThread("");
-        }
-        alert("Thread created successfully!");
-        window.location.reload();
-      })
-      .catch((err) => console.error(err));
-  };
-
+	const createThread = () => {
+		fetch("http://localhost:4000/api/create/thread", {
+			method: "POST",
+			body: JSON.stringify({
+				thread,
+				userId: localStorage.getItem("userId"),
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+			credentials: "include",
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.success) {
+					setThreadList([...threadList, data.thread]);
+					setThread("");
+				}
+				alert("Thread created successfully!");
+				window.location.reload();
+			})
+			.catch((err) => console.error(err));
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -93,6 +92,10 @@ const Home = () => {
 										type="input"
 										placeholder="Title"
 										className="input-thread-title"
+										value={thread}
+										onChange={(e) =>
+											setThread(e.target.value)
+										}
 									/>
 								</Form.Group>
 							</Col>
@@ -106,14 +109,6 @@ const Home = () => {
 								</Button>
 							</Col>
 						</Row>
-						{/* <Form.Group className="mb-3" controlId="textarea">
-							<Form.Label>Description</Form.Label>
-							<Form.Control
-								as="textarea"
-								rows={3}
-								placeholder="Description"
-							/>
-						</Form.Group> */}
 					</Form>
 				</Card.Body>
 			</Card>
